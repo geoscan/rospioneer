d47225e0e83c5b1bd1b3b9999e67cae17d0b2a84
IlyaDanilenko <ilya@eadsoft.com>
2020-12-11 13:23:22 +0400

    Update to 1.1.5

--- src/rospioneer/__init__.py
+++ src/rospioneer/__init__.py
@@ -72,12 +72,14 @@ def _log():
     while not rospy.is_shutdown():
         pass
 
-def _update():
+def _update(argv):
     import urllib.request as request
+    from urllib.error import HTTPError
     from pathlib import Path
     import json
     import xml.etree.ElementTree as etree
     import warnings
+    import pickle
 
     def get_version(pkg_name): 
         version_str = str(subprocess.Popen(["rosversion", pkg_name], stdout=subprocess.PIPE).communicate()[0],'utf-8').replace("\n","").split(" ")
@@ -94,33 +96,56 @@ def _update():
     os.system("sudo apt-get upgrade -y")
     print("\033[94m{}\033[00m" .format("Get paсkages list"))
     update = 0
-    url = "http://api.github.com/repos/geoscan/geoscan_pioneer_max/contents/geoscan_ws/src"
-    url_version = "https://raw.githubusercontent.com/geoscan/{}/{}/package.xml"
+    branch = "master"
+    repos ="http://api.github.com/repos/geoscan/geoscan_pioneer_max"
+    url_branches="{}/branches".format(repos)
+    branches_list = json.loads(str(request.urlopen(url_branches).read(),'utf-8'))
     workspace_dir = "/geoscan_ws/src/"
-    package_list = json.loads(str(request.urlopen(url).read(),'utf-8'))
-    for package in package_list:
-        name = package['name']
-        if package['size'] == 0:
-            sha = package['sha']
-            xml_text = etree.fromstring(str(request.urlopen(url_version.format(str(name),str(sha))).read(),'utf-8'))
-            version = xml_text.getchildren()[1].text
-            print("Check {} paсkage".format(name))
-            current_version = get_version(name)
-            if current_version < version:
-                update += 1
-                print("\tUpdate {} paсkage".format(name))
-                subprocess.call("sudo rm -r "+home+workspace_dir+name,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                subprocess.Popen(["git","clone","https://github.com/geoscan/{}.git".format(name),home+workspace_dir+name],stdout=subprocess.PIPE).communicate()
-                subprocess.call(["cd",home+workspace_dir+name,"&&","git checkout "+sha],shell=True)
-                subprocess.call("sudo rm -r {}/.git".format(home+workspace_dir+name),shell=True)
-            else:
-                print("\tPaсkage {} up to date".format(name))
-    ext_msg = ""
-    if update > 0:
-        print("Start rebuild workspace")
-        os.system("cd {}/geoscan_ws && catkin_make".format(home))
-        ext_msg = " To update the environment run \'source ~/.bashrc\' or open a new terminal session."
-    print("\033[92m{}\033[00m" .format("Updating Geoscan Pioneer Max system complite."+ext_msg))
+    pickle_file = "{}rospioneer/src/rospioneer/branch.pickle".format(home+workspace_dir)
+    try:
+        with open(pickle_file,'rb') as f:
+            branch = pickle.load(f)[0]
+    except:
+        pass
+    change_branch = False
+    if ( len(argv) >= 4 ) and ( ( argv[2] == "--branch" ) or ( argv[2] == "-b" ) ) and any(branch['name'] == argv[3] for branch in branches_list) and ( branch != argv[3] ):
+        branch = argv[3]
+        with open(pickle_file,'wb') as f:
+            pickle.dump([branch],f)
+        change_branch = True
+    print("\033[94m{}\033[00m" .format("Git branch set to {}".format(branch)))
+
+    url = "{}/contents/geoscan_ws/src?ref={}".format(repos,branch)
+    url_version = "https://raw.githubusercontent.com/geoscan/{}/{}/package.xml"
+    
+    try:
+        package_list = json.loads(str(request.urlopen(url).read(),'utf-8'))
+
+        for package in package_list:
+            name = package['name']
+            if package['size'] == 0:
+                sha = package['sha']
+                xml_text = etree.fromstring(str(request.urlopen(url_version.format(str(name),str(sha))).read(),'utf-8'))
+                version = xml_text.getchildren()[1].text
+                print("Check {} paсkage".format(name))
+                current_version = get_version(name)
+                if ( current_version < version ) or change_branch:
+                    update += 1
+                    print("\tUpdate {} paсkage".format(name))
+                    subprocess.call("sudo rm -r "+home+workspace_dir+name,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    subprocess.Popen(["git","clone","https://github.com/geoscan/{}.git".format(name),home+workspace_dir+name],stdout=subprocess.PIPE).communicate()
+                    subprocess.call("cd {} && git checkout {}".format(home+workspace_dir+name,sha),shell=True)
+                    subprocess.call("sudo rm -r {}/.git".format(home+workspace_dir+name),shell=True)
+                else:
+                    print("\tPaсkage {} up to date".format(name))
+        ext_msg = ""
+        if update > 0:
+            print("Start rebuild workspace")
+            os.system("cd {}/geoscan_ws && catkin_make".format(home))
+            ext_msg = " To update the environment run \'source ~/.bashrc\' or open a new terminal session."
+        print("\033[92m{}\033[00m" .format("Updating Geoscan Pioneer Max system complite."+ext_msg))
+    except HTTPError:
+        print("\033[91m{}{}\033[00m".format("geoscan_ws/src not found in branch: ",branch)) 
 
 def _start():
     subprocess.Popen(["roslaunch","gs_core","pioneer.launch","--screen"]).communicate()
@@ -147,18 +172,18 @@ def rospioneermain(argv=None):
     argv = rospy.myargv(argv)
     if len(argv) == 1:
         _info()
-    try:
-        command = argv[1]
-        if command == "start":
-            _start()
-        elif command == "log":
-            _log()
-        elif command == "status":
-            _status()
-        elif command == "camera":
-            _camera()
-        elif command == "update":
-            _update()
-    except:
-        pass
+    # try:
+    command = argv[1]
+    if command == "start":
+        _start()
+    elif command == "log":
+        _log()
+    elif command == "status":
+        _status()
+    elif command == "camera":
+        _camera()
+    elif command == "update":
+        _update(argv)
+    # except Exception as e:
+    #     print(e)
         
